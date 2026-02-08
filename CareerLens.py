5018ca686ce9b24df363832d5f3f6180dad2d601
yashgajera00 <gajerayash2223@gmail.com>
2026-02-08 20:38:53 +0530

    temporary sqlite

--- CareerLens.py
+++ CareerLens.py
@@ -8,7 +8,7 @@ app.secret_key = "abc"
 
 # ================= DATABASE CONFIG =================
 import os
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
